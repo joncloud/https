56b1cf47db16ef9373e9e4d766327820a426b1e1
joncloud <jdberube@gmail.com>
2020-08-05 18:38:58 -0700

    Separates tests out a bit

--- tests/https.Tests/ContentTypeTests.cs
+++ tests/https.Tests/ContentTypeTests.cs
@@ -1,16 +1,14 @@
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Linq;
 using Xunit;
 
 namespace Https.Tests
 {
-    public class IntegrationTests : IClassFixture<WebHostFixture>
+    public class ContentTypeTests : IClassFixture<WebHostFixture>
     {
         readonly WebHostFixture _fixture;
-        public IntegrationTests(WebHostFixture fixture) =>
+        public ContentTypeTests(WebHostFixture fixture) =>
             _fixture = fixture;
 
         [Fact]
@@ -63,20 +61,6 @@ namespace Https.Tests
             ).ToString();
 
             Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public async Task RedirectTest_ShouldShow3XXResponse_GivenStopAutoRedirects()
-        {
-            var args = new[]
-            {
-                "get", "http://localhost:5000/Redirect", "--stop-auto-redirects"
-            };
-
-            var result = await Https.ExecuteAsync(args);
-
-            Assert.Equal("HTTP/1.1 301 Moved Permanently", result.Status);
-            Assert.Equal("http://localhost:5000/Mirror", result.Headers["Location"]);
         }
     }
 }
---
+++ tests/https.Tests/RedirectTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Https.Tests
+{
+    public class RedirectTests : IClassFixture<WebHostFixture>
+    {
+        readonly WebHostFixture _fixture;
+        public RedirectTests(WebHostFixture fixture) =>
+            _fixture = fixture;
+
+        [Fact]
+        public async Task RedirectTest_ShouldShow3XXResponse_GivenStopAutoRedirects()
+        {
+            var args = new[]
+            {
+                "get", $"{_fixture.HttpUrl}/Redirect", "--stop-auto-redirects"
+            };
+
+            var result = await Https.ExecuteAsync(args);
+
+            Assert.Equal("HTTP/1.1 301 Moved Permanently", result.Status);
+            Assert.Equal($"{_fixture.HttpUrl}/Mirror", result.Headers["Location"]);
+        }
+    }
+}
